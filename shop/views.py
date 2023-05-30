c97777f3290749307c9bacd30fa574631664ee68
vinubhat092 <vinayakbhat94@gmail.com>
2023-05-30 22:08:10 +0530

    template inheritance

--- shop/views.py
+++ shop/views.py
@@ -5,14 +5,18 @@ from shop.models import product
 def home(request):
     all_objects = product.objects.all()
     context = {
-        "all_objects":all_objects
+        "all_objects":all_objects,
     }
-    return render(request,"shop/home.html",context=context)
+    return render(request,"shop/home.html",context)
 
 def index(request):
+    all_objects = product.objects.all()
+    context = {
+        "all_objects":all_objects,
+    }
     return render(request,"shop/index.html")
 def about(request):
-    return HttpResponse("we are at about")
+    return render(request,"shop/about.html")
 def contact(request):
     return HttpResponse("we are at contact")
 def tracker(request):
