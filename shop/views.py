aa28912589939a96c2076bab0086472ab3d77e58
vinubhat092 <vinayakbhat94@gmail.com>
2023-06-03 13:28:35 +0530

    now

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from shop.models import product
+from math import ceil
 def home(request):
     all_objects = product.objects.all()
     context = {
@@ -10,11 +11,31 @@ def home(request):
     return render(request,"shop/home.html",context)
 
 def index(request):
-    all_objects = product.objects.all()
-    context = {
-        "all_objects":all_objects,
-    }
-    return render(request,"shop/index.html")
+    products = product.objects.all()
+    n = len(products)
+    nSlides = n//4 + ceil ((n/4)-(n//4))
+    print("asdfg",range(nSlides))
+    # context = {
+    #     "no_of_slides":nSlides,
+    #     "range": range(nSlides),
+    #     "product":products,
+    # }
+    # allprods = [[products,range(1, nSlides), nSlides],
+    #             [products,range(1, nSlides), nSlides]]
+    allprods = []
+    catprods = product.objects.values('category','id')
+    cats = { item['category'] for item in catprods}
+    print("wwd",cats)
+    for cat in cats:
+        products = product.objects.filter(category=cat)
+        n = len(products)
+        nSlides = n//4 + ceil ((n/4)-(n//4))
+        allprods.append([products])
+    print("fsf",allprods)
+    context = {"allprods":allprods,
+               "product":products,}
+
+    return render(request,"shop/index.html",context)
 def about(request):
     return render(request,"shop/about.html")
 def contact(request):
